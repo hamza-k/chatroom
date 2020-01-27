a1042f6e9960f894740f0d1a48d9d7d9b9ea10a8
Hamza KHADRI <hamza.khadri@hotmail.com>
2020-01-27 21:22:18 +0100

    main design and developping username system

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
+// Module calling
 require('babel-register')
 const app = require('express')()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http);
 
-app.get('/', function(req, res){
+// Page displaying
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
   })
 
-io.on('connection', (socket) => {
-    socket.on('chat message', function(msg){
+// Socket.io computing when connection
+io.on('connection', socket => {
+
+    // Message when new user is connected
+    socket.on('new user', newUser => {
+        console.log(newUser + ' is online now !')
+        io.emit('new user', newUser)
+    })
+
+    // Message sending
+    socket.on('chat message', msg => {
         io.emit('chat message', msg);    
+    })
+
+    // Username changing
+    socket.on('username change', data => {
+        io.emit('username change', data)
     })
   })
 
-http.listen(8080, function(){
+// Server creating
+http.listen(8080, () => {
   console.log('listening on *:8080')
 })
