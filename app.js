63f8648396a15c9f55a46bfd9925e83687c0ce4e
Hamza KHADRI <hamza.khadri@hotmail.com>
2020-02-02 17:16:06 +0100

    created log in and log out event, creating and sorting online user list

--- app.js
+++ app.js
@@ -9,24 +9,63 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
   })
 
+
+  function compare( a, b ) {
+    if ( a.username < b.username){
+      return -1;
+    }
+    if ( a.username > b.username ){
+      return 1;
+    }
+    return 0;
+  }
+
+var usersList = []
+
 // Socket.io computing when connection
 io.on('connection', socket => {
 
     // Message when new user is connected
     socket.on('new user', newUser => {
-        console.log(newUser + ' is online now !')
+        //console.log(newUser + ' is online now !')
         io.emit('new user', newUser)
     })
 
     // Message sending
     socket.on('chat message', msg => {
-        io.emit('chat message', msg);    
+        io.emit('chat message', msg)
     })
 
     // Username changing
     socket.on('username change', data => {
         io.emit('username change', data)
+        for (var i in usersList) {
+            if (usersList[i].id == data.id) {
+               usersList[i].username = data.newUsername;
+               break
+            }
+        }
+        usersList.sort(compare)
+        io.emit('load list', usersList)
     })
+
+    // New in connected user list
+    socket.on('new in list', obj => {
+        //console.log(obj)
+        usersList.push(obj)
+        usersList.sort(compare)
+        //console.log(usersList)
+        io.emit('load list', usersList)
+    })
+
+    socket.on('exit user', obj => {
+        //console.log(obj.username + ' is gone')
+        io.emit('exit user', obj)
+        usersList = usersList.filter(el => el.id != obj.id)
+        //console.log(usersList)
+        io.emit('load list', usersList)
+    })
+
   })
 
 // Server creating
